4c478e270e86038aff2b55c3cf0218b96c7efd3f
achernomyrdin <achernomyrdin@IMAC-201-101-PC.itf.local>
2023-03-01 18:32:00 +0500

    daily commit

---
+++ src/lesson2/Exercises.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Exercises {
+	static int x = 0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. ЗАпустить два потока которые пооеердно увелилчвают 
+		// значение поля x
+		// 2. Запустить два потока которые считают сумму текущего
+		// значения x 
+		// 3. В конце каждый из потоков выдает накопленную сумму
+	}
+
+}
--- src/lesson2/SyncTest.java
+++ src/lesson2/SyncTest.java
@@ -1,10 +1,43 @@
 package lesson2;
 
 public class SyncTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int x = 0;
+	static final Object	sync = new Object();
+	
+	public static void main(String[] args) throws InterruptedException {
+		final Thread[]	t = new Thread[10];
+		
+		for (int index = 0; index < t.length; index++) {
+			t[index] = new Thread(()->increment());
+		}
+		for(Thread item : t) {
+			item.start();
+		}
+		for(Thread item : t) {
+			item.join();
+		}
+		System.err.println("x="+x);
 	}
 
+	private static void increment() {
+		for(int index = 0; index < 100000; index++) {
+//			synchronized(sync) { // try { aload sync; monitorenter
+//				x++;
+//			}                    // } finally {aload sync; monitorexit;}
+			//x = x + 1;
+			inc();
+		}
+	}
+	
+	private static synchronized void inc() {
+		// synchronized(SyncTest.class) {
+		x++;
+		// }
+	}
+	
+	private synchronized void nonstatic() {
+		// syncronized(this) {
+		// TODO: ......
+		// }
+	}
 }
